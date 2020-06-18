5ea10ef19f9a3cab15cdd2129435b1a98316d110
María Paz López <maria.lopez@campusdual.com>
2020-06-18 16:53:05 +0200

    Añadiendo el ejercicio bankmanager-boot

--- frontend/src/main/ngx/src/app/main/main-routing.module.ts
+++ frontend/src/main/ngx/src/app/main/main-routing.module.ts
@@ -4,10 +4,16 @@ import { AuthGuardService } from 'ontimize-web-ngx';
 
 import { MainComponent } from './main.component';
 import { HomeModule } from './home/home.module';
+import { CustomersModule } from './customers/customers.module';
 
 export function loadHomeModule() {
   return HomeModule;
 }
+
+export function loadCustomersModule() {
+  return CustomersModule;
+}
+
 
 export const routes: Routes = [
   {
@@ -19,6 +25,10 @@ export const routes: Routes = [
       {
         path: 'home',
         loadChildren: loadHomeModule
+      },
+      {
+        path: 'customers',
+        loadChildren: loadCustomersModule
       }
     ]
   }
